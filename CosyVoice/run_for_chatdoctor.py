07a52a5678a5185dd966d1f35cda2e2004a008b8
Abhishekdileep <abhishekdileep99@gmail.com>
2026-08-24 12:32:52 +0530

    updated generation pipeline

--- CosyVoice/run_for_chatdoctor.py
+++ CosyVoice/run_for_chatdoctor.py
@@ -3,6 +3,7 @@ sys.path.append('third_party/Matcha-TTS')
 from cosyvoice.cli.cosyvoice import AutoModel
 import torchaudio
 import json
+import torch 
 import random
 import pandas as pd 
 import os
@@ -34,8 +35,11 @@ for j,i in enumerate(df.iterrows()):
 
 cosyvoice = AutoModel(model_dir='pretrained_models/CosyVoice2-0.5B')
 for i, sentence in enumerate(samples):
+    tt_audio = []
     for  j in cosyvoice.inference_zero_shot(sentence['input'], '希望你以后能够做的比我还好呦。', './asset/zero_shot_prompt.wav'):
-        torchaudio.save('./chatdoctor/zero_shot_{}.wav'.format(i), j['tts_speech'], cosyvoice.sample_rate)
+        tt_audio.append(j['tts_speech'])
+    tt_audio = torch.cat(tt_audio, dim=1)
+    torchaudio.save('./chatdoctor/zero_shot_{}.wav'.format(i),  tt_audio ,cosyvoice.sample_rate)
 
 
 with open("./chatdoctor/samples.json", "w", encoding="utf-8") as file:
--- CosyVoice/run_for_medqa.py
+++ CosyVoice/run_for_medqa.py
@@ -1,4 +1,5 @@
 import sys
+import torch
 sys.path.append('third_party/Matcha-TTS')
 from cosyvoice.cli.cosyvoice import AutoModel
 import torchaudio
@@ -17,14 +18,17 @@ def load_random_samples(path, num_samples=30):
     return random.sample(records, min(num_samples, len(records)))
 
 import os
-os.makedirs('./medqa_in/', exist_ok=True)
-with open('./medqa_in/samples.json', "w+", encoding="utf-8") as file:
+os.makedirs('./medqa/', exist_ok=True)
+with open('./medqa/samples.json', "w+", encoding="utf-8") as file:
         json.dump('', file, ensure_ascii=False, indent=2)
-samples = load_random_samples('/DATA/carer.ai/data/medqa/data/questions/en_5/dev.jsonl', num_samples=250)
+samples = load_random_samples('/DATA/carer.ai/data/medqa/data/questions/en_5/dev.jsonl', num_samples=50)
 cosyvoice = AutoModel(model_dir='pretrained_models/CosyVoice2-0.5B')
 for i, sentence in enumerate(samples):
     complete_sentence = f"{sentence['question']} Answer A: {sentence['A']}, Answer B: {sentence['B']}, Answer C: {sentence['C']}, Answer D: {sentence['D']}, Answer E: {sentence['E']}" 
-    for  j in cosyvoice.inference_zero_shot(complete_sentence, '希望你以后能够做的比我还好呦。', './asset/zero_shot_inen_prompt.wav'):
-        torchaudio.save('./medqa_in/zero_shot_{}.wav'.format(i+50), j['tts_speech'], cosyvoice.sample_rate)
-    with open('./medqa_in/samples.json', "a+", encoding="utf-8") as file:
+    tt_audio = []
+    for  j in cosyvoice.inference_zero_shot(complete_sentence, '希望你以后能够做的比我还好呦。', './asset/zero_shot_prompt.wav'):
+        tt_audio.append(j['tts_speech'])
+    tt_audio = torch.cat(tt_audio, dim=1)
+    torchaudio.save('./medqa/zero_shot_{}.wav'.format(i+50),tt_audio, cosyvoice.sample_rate)
+    with open('./medqa/samples.json', "a+", encoding="utf-8") as file:
         json.dump(sentence, file, ensure_ascii=False, indent=2)
